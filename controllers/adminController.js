cfd4dee2e1fbbc218aebe3551d0bcdf85851c471
Abdallah Muhammed <abdallahiifox@gmail.com>
2023-07-03 03:16:46 +0300

    add:verify freelancer and client by admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -62,32 +62,83 @@ export const deactiveFreelancerById = async (request, response, next) => {
   }
 };
 
+export const deactiveClientById = async (request, response, next) => {
+  const { clientId } = request.body;
 
-export const deactiveClientById = async (request,response,next)=>{
-    const { clientId } = request.body;
+  if (!clientId) {
+    return response
+      .status(401)
+      .json({ error: "please enter a valid clientId" });
+  }
 
-    if (!clientId) {
-      return response.status(401).json({ error: "please enter a valid clientId" });
+  try {
+    const deactiveClient = await ClientModel.updateOne({ _id: clientId }, [
+      {
+        $set: { isActive: { $not: "$isActive" } },
+      },
+    ]);
+
+    if (!deactiveClient) {
+      return response.status(404).json({ error: "can not find this client" });
     }
-  
-    try {
-      const deactiveClient = await ClientModel.updateOne(
-        { _id: clientId },
-        [
-          {
-            $set: { isActive: { $not: "$isActive" } },
-          },
-        ]
-      );
-  
-      if (!deactiveClient) {
-        return response
-          .status(404)
-          .json({ error: "can not find this client" });
-      }
-      response.status(200).json({ message: "Sucssess" });
-    } catch (error) {
-      error.statusCode = 500;
-      next(error);
+    response.status(200).json({ message: "Sucssess" });
+  } catch (error) {
+    error.statusCode = 500;
+    next(error);
+  }
+};
+
+export const verifyFreelancerById = async (request, response, next) => {
+  const { freelancerId } = request.body;
+
+  if (!freelancerId) {
+    return response.status(401).json({ error: "please enter a valid Id" });
+  }
+
+  try {
+    const verifyFreelancer = await FreelancerModel.updateOne(
+      { _id: freelancerId },
+      [
+        {
+          $set: { isVerify: { $not: "$isVerify" } },
+        },
+      ]
+    );
+
+    if (!verifyFreelancer) {
+      return response
+        .status(404)
+        .json({ error: "can not find this freelancer" });
     }
-}
+    response.status(200).json({ message: "Sucssess" });
+  } catch (error) {
+    error.statusCode = 500;
+    next(error);
+  }
+};
+
+export const verifyClientById = async (request, response, next) => {
+  const { clientId } = request.body;
+
+  if (!clientId) {
+    return response
+      .status(401)
+      .json({ error: "please enter a valid clientId" });
+  }
+
+  try {
+    const verifyClient = await ClientModel.updateOne({ _id: clientId }, [
+      {
+        $set: { isVerified: { $not: "$isVerified" } },
+      },
+    ]);
+
+    if (!verifyClient) {
+      return response.status(404).json({ error: "can not find this client" });
+    }
+    response.status(200).json({ message: "Sucssess" });
+  } catch (error) {
+    error.statusCode = 500;
+    next(error);
+  }
+};
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,12 +1,18 @@
-import  express  from "express";
-import { loginAdmin,getAllStatistics,deactiveFreelancerById,deactiveClientById } from "../controllers/adminController.js";
+import express from "express";
+import {
+  loginAdmin,
+  getAllStatistics,
+  deactiveFreelancerById,
+  deactiveClientById,
+  verifyFreelancerById,
+  verifyClientById,
+} from "../controllers/adminController.js";
 
-export const adminRoute = express.Router()
+export const adminRoute = express.Router();
 
-
-adminRoute.post('/',loginAdmin)
-adminRoute.get('/statistics',getAllStatistics)
-adminRoute.patch('/deactive-freelancer',deactiveFreelancerById)
-adminRoute.patch('/deactive-client',deactiveClientById)
-
-
+adminRoute.post("/", loginAdmin);
+adminRoute.get("/statistics", getAllStatistics);
+adminRoute.patch("/deactive-freelancer", deactiveFreelancerById);
+adminRoute.patch("/deactive-client", deactiveClientById);
+adminRoute.patch("/verify-freelancer", verifyFreelancerById);
+adminRoute.patch("/verify-client", verifyClientById);
