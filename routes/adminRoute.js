a0f335929ba9c84ff5dbee9ae70193ef620381b1
Mr Robot <hemdan219@gmail.com>
2023-07-04 12:26:56 +0300

    Adding Login

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -13,18 +13,55 @@ import {
   loginAdmin,
   registerAdmin,
   addSuperAdmin,
+  protectMiddleware,
+  allowedTo,
 } from "../controllers/adminAuthController.js";
 export const adminRoute = express.Router();
-
+const roles = ["superAdmin", "admin"];
 adminRoute.post("/auth/admin", addSuperAdmin);
-adminRoute.post("/auth/register", registerAdmin);
+adminRoute.post(
+  "/auth/register",
+  protectMiddleware,
+  allowedTo(["superAdmin"]),
+  registerAdmin
+);
 adminRoute.post("/auth/login", loginAdmin);
 
-adminRoute.get("/clients", getAllClients);
+adminRoute.get("/clients", protectMiddleware, allowedTo(roles), getAllClients);
 
-adminRoute.get("/freelancers", getAllFreelancers);
-adminRoute.get("/statistics", getAllStatistics);
-adminRoute.patch("/deactive-freelancer", deactiveFreelancerById);
-adminRoute.patch("/deactive-client", deactiveClientById);
-adminRoute.patch("/verify-freelancer", verifyFreelancerById);
-adminRoute.patch("/verify-client", verifyClientById);
+adminRoute.get(
+  "/freelancers",
+  protectMiddleware,
+  allowedTo(roles),
+  getAllFreelancers
+);
+adminRoute.get(
+  "/statistics",
+  protectMiddleware,
+  allowedTo(roles),
+  getAllStatistics
+);
+adminRoute.patch(
+  "/deactive-freelancer",
+  protectMiddleware,
+  allowedTo(roles),
+  deactiveFreelancerById
+);
+adminRoute.patch(
+  "/deactive-client",
+  protectMiddleware,
+  allowedTo(roles),
+  deactiveClientById
+);
+adminRoute.patch(
+  "/verify-freelancer",
+  protectMiddleware,
+  allowedTo(roles),
+  verifyFreelancerById
+);
+adminRoute.patch(
+  "/verify-client",
+  protectMiddleware,
+  allowedTo(roles),
+  verifyClientById
+);
